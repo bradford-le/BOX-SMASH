787bae14748670e36c70b2a6ddf03ba7b4d669df
Brad Le <brad.le444@gmail.com>
2017-06-28 13:41:05 +0200

    Fix generate player

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function Game () {
 
   this.Boxes = [];
-  this.level = 10; //freezes with more than 10 boxes
+  this.level = 15; //freezes at 10 boxes
   this.boxTypes = ["key","math","riddle"];
   var type = "";
 
@@ -99,7 +99,7 @@ Game.prototype.generatePlayer = function() {
   do {
     this.player = {
       row: Math.floor(Math.random() * 10),
-      column: Math.floor(Math.random() * 10)
+      col: Math.floor(Math.random() * 10)
     };
     console.log("Player attempts");
   } while (this.checkCollsion(this.player));
@@ -107,7 +107,7 @@ Game.prototype.generatePlayer = function() {
 
 Game.prototype.drawPlayer = function() {
   var selector = '[data-row=' + this.player.row + ']' +
-                 '[data-col=' + this.player.column + ']';
+                 '[data-col=' + this.player.col + ']';
   $(selector).addClass('player');
 };
 
@@ -121,37 +121,45 @@ Game.prototype.playerMovement = function() {
       case 38: // arrow up
       this.player = {
         row: (this.player.row-1+10)%10,
-        column: this.player.column
+        col: this.player.col
       };
-        this.checkKeyCollision();
+        this.checkBoxFound();
+        // this.clearPlayer();
+        // this.drawPlayer();
         break;
       case 40: // arrow down
       this.player = {
         row: (this.player.row+1+10)%10,
-        column: this.player.column
+        col: this.player.col
       };
-        this.checkKeyCollision();
+        this.checkBoxFound();
+      // this.clearPlayer();
+      // this.drawPlayer();
         break;
       case 37: // arrow left
       this.player = {
         row: this.player.row,
-        column: (this.player.column-1+10)%10
+        col: (this.player.col-1+10)%10
       };
-        this.checkKeyCollision();
+        this.checkBoxFound();
+      // this.clearPlayer();
+      // this.drawPlayer();
         break;
       case 39: // arrow right
       this.player = {
         row: this.player.row,
-        column: (this.player.column+1+10)%10
+        col: (this.player.col+1+10)%10
       };
-        this.checkKeyCollision();
+        this.checkBoxFound();
+      // this.clearPlayer();
+      // this.drawPlayer();
         break;
         }
     }.bind(this)); //bind(this) allows row to be for player.row to be used in switch
 };
 
 Game.prototype.start = function() {
-  for(i = this.level; i>0 ; i--) {
+  for(var i = this.level; i>0 ; i--) {
     var randomBox = this.boxTypes[Math.floor(Math.random()*3)];
     if(randomBox==="key"){
       this.generateKeyBox();
@@ -166,80 +174,29 @@ Game.prototype.start = function() {
   }
       this.generatePlayer();
       this.drawPlayer();
+      this.playerMovement();
 };
 
-Game.prototype.checkCollsion = function(box) {
-  return this.Boxes.some(function(currentBox) {
-    return currentBox.row === box.row && currentBox.column === box.column;
+Game.prototype.checkCollsion = function(box) { //box is newBox
+  return this.Boxes.some(function(currentBox,index) { //currentBox is first element in Boxes array
+    return currentBox.row === box.row && currentBox.col === box.col;
   });
 };
 
-// Game.prototype.checkKeyCollision = function(){
-//   if(this.keyBoxFound(this.keyBox)){
-//     this.clearKeyBox();
-//     this.clearPlayer();
-//     this.drawPlayer();
-//     setTimeout(function() {
-//       keyInput = prompt("Hit " + this.keyBox.key + " " + this.keyBox.number + " times");}.bind(this),200);
-//   } else {
-//     this.clearPlayer();
-//     this.drawPlayer();
-//   }
-// };
-
-
-//
-// Game.prototype.playerMovement = function() {
-//   $('body').on('keydown', function(e) {
-//     switch (e.keyCode) {
-//       case 38: // arrow up
-//       this.player = {
-//         row: (this.player.row-1+10)%10,
-//         column: this.player.column
-//       };
-//         this.checkKeyCollision();
-//         break;
-//       case 40: // arrow down
-//       this.player = {
-//         row: (this.player.row+1+10)%10,
-//         column: this.player.column
-//       };
-//         this.checkKeyCollision();
-//         break;
-//       case 37: // arrow left
-//       this.player = {
-//         row: this.player.row,
-//         column: (this.player.column-1+10)%10
-//       };
-//         this.checkKeyCollision();
-//         break;
-//       case 39: // arrow right
-//       this.player = {
-//         row: this.player.row,
-//         column: (this.player.column+1+10)%10
-//       };
-//         this.checkKeyCollision();
-//         break;
-//         }
-//     }.bind(this)); //bind(this) allows row to be for player.row to be used in switch
-// };
-//
-// Game.prototype.keyBoxFound = function(keyBoxPosition){
-//   return this.player.row === keyBoxPosition.row && this.player.column === keyBoxPosition.column;
-//   };
-//
-// Game.prototype.checkKeyCollision = function(){
-//   if(this.keyBoxFound(this.keyBox)){
-//     this.clearKeyBox();
-//     this.clearPlayer();
-//     this.drawPlayer();
-//     setTimeout(function() {
-//       keyInput = prompt("Hit " + this.keyBox.key + " " + this.keyBox.number + " times");}.bind(this),200);
-//   } else {
-//     this.clearPlayer();
-//     this.drawPlayer();
-//   }
-// };
+Game.prototype.checkBoxFound = function(){
+  if(this.checkCollsion(this.player)){
+    console.log("COLLISION!");
+    this.clearKeyBox();
+    this.clearPlayer();
+    this.drawPlayer();
+    setTimeout(function() {
+      keyInput = prompt("Hit " + this.keyBox.key + " " + this.keyBox.number + " times");}.bind(this),200);
+  } else {
+    console.log("NO COLLISION!");
+    this.clearPlayer();
+    this.drawPlayer();
+  }
+};
 
 var game;
 $(document).ready(function() {
